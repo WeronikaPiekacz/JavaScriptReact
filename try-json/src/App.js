fdf4541150ffcc8cdb64a503c42c193e6f67bef2
Weronika Piekacz <weronika.piekacz@interia.pl>
2020-11-15 14:11:20 +0100

    Try-Json

---
+++ try-json/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import withListLoading from './Components/withListLoading';
+import List from './Components/list';
+
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/WeronikaPiekacz/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+    </div>
+  );
+}
+export default App;
