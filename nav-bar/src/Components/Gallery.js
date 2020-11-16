71b14c1c7549c7ddd6e84c657563cdc0c088b2ee
Weronika Piekacz <weronika.piekacz@interia.pl>
2020-11-16 14:56:16 +0100

    API img

--- nav-bar/src/Components/Gallery.js
+++ nav-bar/src/Components/Gallery.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PhotoContainer from './PhotoContainer'
+import '../App.css'
 
-function Gallery() {
-    return (
-        <div className="Home">
-            Gallery
-        </div>
-    )
+class Gallery extends Component {
+    constructor() { 
+        super();
+        this.state = {
+            photos: []
+        };
+    }
+    
+    componentDidMount() {
+        fetch('https://api.thedogapi.com/v1/images/search')
+            .then (response => {
+                if(!response.ok){throw Error("Error fetching dog")}
+                return response.json()
+
+                .then(allData => {
+                    this.setState({photos: allData});
+                })
+
+                .catch(er=>{
+                    throw Error(er.message);
+                })
+            }
+        );      
+    }
+
+    render () {
+        return (
+            <div className='Home'>
+                <div className='Gallery'>
+                    <PhotoContainer photos={this.state.photos}/> 
+                    {this.state.photos.name}
+                </div>
+
+            </div>
+        );
+    }   
+
 }
+
+
+// function Gallery() {
+//     return (
+//         <div className="Home">
+//             Gallery
+//         </div>
+//     )
+// }
 
 export default Gallery
